e94ae626ee776de23b2ab9a4eabd6fd695c007d6
ZiaadNegm <ziaad.negmel-din@student.uva.nl>
2025-06-08 13:23:34 +0200

    Login redirect tested

--- chatbot/src/RoutesDefined.tsx
+++ chatbot/src/RoutesDefined.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import ChatWindow from "./App";
+import LoginPage from "./loginPage";
 
 const RoutesDefined = () => {
   return (
     <Routes>
       <Route path="/" element={<ChatWindow />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
