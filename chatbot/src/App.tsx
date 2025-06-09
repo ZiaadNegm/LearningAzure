dff368786f1def90257acb9e6293bb1bc34f12a6
ZiaadNegm <ziaad.negmel-din@student.uva.nl>
2025-06-09 14:40:06 +0200

    removed base URL

--- chatbot/src/App.tsx
+++ chatbot/src/App.tsx
@@ -1,12 +1,6 @@
 import "./App.css";
 import { ChatWindow } from "./components/ChatWindow";
 
-const base = import.meta.env.VITE_API_BASE_URL;
-
-if (!base) {
-  throw new Error("VITE_API_BASE_URL is not defined.");
-}
-
 function App() {
   return <ChatWindow />;
 }
