8b1ee7cc19b4718cf306ddca5d7bda7a83dc58ec
ZiaadNegm <ziaad.negmel-din@student.uva.nl>
2025-07-01 15:47:07 +0200

    Changed to parmas instead of query

--- api/src/DBuserMetaData.ts
+++ api/src/DBuserMetaData.ts
@@ -11,7 +11,7 @@ const cosmosInput = input.cosmosDB({
   connection: "CosmosDBConnection",
   databaseName: "Database-ziaadsChatbot",
   containerName: "cosmos-container-user-meta-data",
-  sqlQuery: "SELECT * from c WHERE c.userid = {query.userid}",
+  sqlQuery: "SELECT * from c WHERE c.userid = {params.userid}",
 });
 
 interface metaDataStructure {
