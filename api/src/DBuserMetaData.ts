d9ceb536658d5555c2b4371c4d0d893796ebf77e
ZiaadNegm <ziaad.negmel-din@student.uva.nl>
2025-07-30 14:20:04 +0200

    Changed the search from a partitionkey to a sqlquery in which we specify the useridExists

--- api/src/DBuserMetaData.ts
+++ api/src/DBuserMetaData.ts
@@ -14,28 +14,28 @@ export const cosmosInputChatCheckUserID = input.cosmosDB({
   connection: "CosmosDBConnection",
   databaseName: "Database-ziaadsChatbot",
   containerName: "cosmos-container-chats",
-  partitionKey: "{Query.useridExists}",
+  sqlQuery: "SELECT * FROM c WHERE c.userid = {useridExists}",
 });
 
 export const cosmosInputMetaCheckUserID = input.cosmosDB({
   connection: "CosmosDBConnection",
   databaseName: "Database-ziaadsChatbot",
   containerName: "cosmos-container-user-meta-data",
-  partitionKey: "{Query.useridExists}",
+  sqlQuery: "SELECT * FROM c WHERE c.userid = {useridExists}",
 });
 
 export const cosmosInputMetaData = input.cosmosDB({
   connection: "CosmosDBConnection",
   databaseName: "Database-ziaadsChatbot",
   containerName: "cosmos-container-user-meta-data",
-  partitionKey: "{Query.userid}",
+  sqlQuery: "SELECT * FROM c WHERE c.userid = {userid}",
 });
 
 export const cosmosInputChats = input.cosmosDB({
   connection: "CosmosDBConnection",
   databaseName: "Database-ziaadsChatbot",
   containerName: "cosmos-container-chats",
-  partitionKey: "{Query.userid}",
+  sqlQuery: "SELECT * FROM c WHERE c.userid = {userid}",
 });
 
 interface metaDataStructure {
